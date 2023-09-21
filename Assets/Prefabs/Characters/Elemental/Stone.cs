9c3bcf5a89f892844ae273c7e252620d2102774d
poongwyal <144191203+poongwyal@users.noreply.github.com>
2023-09-21 16:13:14 +0900

    몬스터 Trail 및 Hit Box 완료

---
+++ Assets/Prefabs/Characters/Elemental/Stone.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : Character
+{
+    [SerializeField] Collider hitBox;
+    [SerializeField] ParticleSystem trail;
+
+    public void StartHitbox() { hitBox.enabled = true; }    //히트박스 활성화
+    public void EndHitbox() { hitBox.enabled = false; }     //히트박스 비활성화
+
+    public void StartTrail() { trail.Play(); }  //트레일 활성화
+    public void EndTrail() { trail.Stop(); }    //트레일 비활성화
+
+    public override void Init()
+    {
+        hitBox.enabled = false;
+        trail.Stop();
+    }
+}
---
+++ Assets/Prefabs/Characters/Grunt/Grunt.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grunt : Character
+{
+    [SerializeField] Collider[] hitBoxs;
+    [SerializeField] ParticleSystem[] trails;
+
+    public void StartHitbox(int index)
+    {
+        hitBoxs[index].enabled = true;
+    }
+    public void EndHitbox(int index)
+    {
+        hitBoxs[index].enabled = false;
+    }
+
+    public void StartTrail(int index)
+    {
+        trails[index].Play();
+    }
+    public void EndTrail(int index)
+    {
+        trails[index].Stop();
+    }
+
+    public override void Init()
+    {
+        for (int i = 0; i < hitBoxs.Length; i++)
+        {
+            hitBoxs[i].enabled = false;
+        }
+        for (int i = 0; i < trails.Length; i++)
+        {
+            trails[i].Stop();
+        }
+    }
+}
